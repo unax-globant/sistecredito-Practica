f92a11e753aa5e2bab9209144fedc81115474c98
Unax Lezama <unax.lezama@globant.com>
2025-05-28 11:27:21 +0200

    actualizacion a swiper

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import valuesImg from './images/valuesPhone.png';
 import icon1 from './images/icon1.png';
 import icon2 from './images/icon2.png';
 import icon3 from './images/icon3.png';
-import videoTest from './videos/videoTest.mp4';
+
 
 import './App.css';
 
