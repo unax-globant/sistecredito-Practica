5f54063b9d3c0365f0444d4e3cdd60c2f1b306e0
Unax Lezama <unax.lezama@globant.com>
2025-06-03 11:23:59 +0200

    Actualizacion estilos

--- src/App.jsx
+++ src/App.jsx
@@ -87,6 +87,10 @@ function App() {
         />
 
         <Team
+          title="Así es"
+          highlight="nuestro equipo"
+          subtitle="Más de 800 personas con un propósito en común:"
+          subtitleHighlight="Hacerlo posible."
           members={[
             {
               id: "1",
